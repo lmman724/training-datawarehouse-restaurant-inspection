b1000178167e4d5c058431c901a53246bf3a16b6
Man LE <lmman724@outlook.com>
2023-06-05 22:38:22 +0700

    update scripts in connect sql server

--- insert-data-sql-server/connect_SQL_server.py
+++ insert-data-sql-server/connect_SQL_server.py
@@ -14,6 +14,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 ###########################
+
 #test exampple get data from API
 
 # def get_restaurant_inspection_data(url):
@@ -30,7 +31,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 #         return None
 
 # # URL for restaurant inspection data
-# url_restaurant_inspection = 'https://data.cityofnewyork.us/resource/43nn-pn8j.csv'
+# url_restaurant_inspection = 'https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv'
 
 # # Call the function to get the restaurant inspection data
 # data_restaurant_inspection = get_restaurant_inspection_data(url_restaurant_inspection)
@@ -47,32 +48,32 @@ current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 #get full data from API
 def retrieve_all_restaurant_inspection_data():
-    url_restaurant_inspection = 'https://data.cityofnewyork.us/resource/43nn-pn8j.json'
-    limit = 1000  # Number of records to retrieve per request
-    offset = 0  # Initial offset
+    url_restaurant_inspection = 'https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv'
+    # limit = 1000  # Number of records to retrieve per request
+    # offset = 0  # Initial offset
 
-    all_data_restaurant_inspection = []
+    # data_restaurant_inspection = []
 
-    while True:
-        # Make the API request
-        response = requests.get(url_restaurant_inspection, params={'$limit': limit, '$offset': offset})
+    # Make the API request
+    response = requests.get(url_restaurant_inspection)
 
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the csv data from the response
-            data = response.json()
-            all_data_restaurant_inspection.extend(data)
-            logging.info(f"Data has been retrieval, status: {len(all_data_restaurant_inspection)}")
-            
-            # Check if there are more records to retrieve
-            if len(data) < limit:
-                logging.info('Data retrieval completed.')
-                break  # All data has been retrieved
-            else:
-                offset += limit  # Move to the next batch
-        else:
-            logging.error('Error occurred while retrieving data:', response.status_code)
-            break
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the csv data from the response
+        data = response.text
+        reader = csv.DictReader(data.splitlines())
+        all_data_restaurant_inspection = list(reader)
+        logging.info(f"Data has been retrieval, status: {len(all_data_restaurant_inspection)}")
+        
+        # Check if there are more records to retrieve
+    #     if len(data) < limit:
+    #         logging.info('Data retrieval completed.')
+    #         break  # All data has been retrieved
+    #     else:
+    #         offset += limit  # Move to the next batch
+    # else:
+    #     logging.error('Error occurred while retrieving data:', response.status_code)
+    #     break
 
     return all_data_restaurant_inspection
 
@@ -91,7 +92,7 @@ def save_file_as_structure(dataset):
     os.makedirs(folder_structure, exist_ok=True)
     
     #save the retrieved data to a json file
-    restaurant_inspection_file_path = folder_structure + "/" + current_date.strftime("%Y_%m_%d") + "_restaurant_inspection.csv"
+    restaurant_inspection_file_path = folder_structure + "/" + current_date.strftime("%Y_%m_%d_%H_%M") + "_restaurant_inspection.csv"
     with open(restaurant_inspection_file_path, 'w', newline= "", encoding= 'utf-8') as file:
         write = csv.writer(file)
         write.writerow(dataset[0].keys())
@@ -126,8 +127,8 @@ def connectSqlServer(Server, Database, Port, User, Password):
 
     except Exception as e:
         print("An error occurred when connecting to DB, error details: {}".format(e))
-        return False, None
-    
+        return False, None, None
+
 # Define your server, database, port, username, and password
 server = '127.0.0.1'
 database = 'DWH_Staging'
@@ -141,59 +142,65 @@ success,conn,cursor = connectSqlServer(server, database, port, username, passwor
 
 if success:
     try:
-    
+        #open the local csv file
+        with open(csv_restaurant_inspection_file_path, 'r', encoding='utf-8', newline='') as csvfile:
+             # Create a CSV reader object
+            reader = csv.DictReader(csvfile)
         # Iterate over each row in the CSV file
-        for row in data_restaurant_inspection:
-            # Extract the values from the row
-            camis = row['camis'].replace("'", "''")
-            dba = row['dba'].replace("'", "''")
-            boro = row['boro'].replace("'", "''")
-            building = row['building'].replace("'", "''")
-            street = row['street'].replace("'", "''")
-            zipcode = row['zipcode'].replace("'", "''")
-            phone = row['phone'].replace("'", "''")
-            cuisine_description = row['cuisine_description'].replace("'", "''")
-            inspection_date = row['inspection_date'].replace("'", "''")
-            action = row['action'].replace("'", "''")
-            violation_code = row['violation_code'].replace("'", "''")
-            violation_desc = row['violation_description'].replace("'", "''")
-            critical_flag = row['critical_flag'].replace("'", "''")
-            score = row['score'].replace("'", "''")
-            grade = row['grade'].replace("'", "''")
-            grade_date = row['grade_date'].replace("'", "''")
-            record_date = row['grade_date'].replace("'", "''")
-            inspection_type = row['inspection_type'].replace("'", "''")
-            latitude = row['latitude'].replace("'", "''")
-            longitude = row['longitude'].replace("'", "''")
-            community_board = row['community_board'].replace("'", "''")
-            council_district = row['council_district'].replace("'", "''")
-            census_tract = row['census_tract'].replace("'", "''")
-            bin_number = row['bin'].replace("'", "''")
-            bbl_number = row['bbl'].replace("'", "''")
-            nta = row['nta'].replace("'", "''")
-            # try:
-            #     encoded_camis = camis.encode('utf-8', errors='ignore')
-            #     encoded_dba = dba.encode('utf-8', errors='ignore')
-            # except UnicodeEncodeError:
-            #     # Handle or log the error as needed
-            #     continue
-            # Generate the SQL INSERT statement
-            sql = f"INSERT INTO dbo.restaurant_inspection (camis, dba, boro, building, street, zipcode, phone, cuisine, inspection_date, action, violation_code, violation_desc, critical_flag, score, grade, grade_date, record_date, inspection_type, latitude, longitude, community_board, council_district, census_tract, bin_number, bbl_number, nta) VALUES ('{camis}', '{dba}', '{boro}', '{building}', '{street}', '{zipcode}', '{phone}', '{cuisine_description}', '{inspection_date}', '{action}', '{violation_code}', '{violation_desc}', '{critical_flag}', '{score}', '{grade}', '{grade_date}', '{record_date}', '{inspection_type}', '{latitude}', '{longitude}', '{community_board}', '{council_district}', '{census_tract}', '{bin_number}', '{bbl_number}', '{nta}')"
-            # Execute the insert query
-            cursor.execute(sql)
-            # Print the status of the row insertion
-            logging.info(f"Row inserted successfully: {row}")
-            # pause execution for 10 second
-            time.sleep(0.01)
-        # Commit the changes to the database
-        cursor.commit()
+            for row in reader:
+                # Extract the values from the row
+                camis = row['CAMIS'].replace("'", "''")
+                dba = row['DBA'].replace("'", "''")
+                boro = row['BORO'].replace("'", "''")
+                building = row['BUILDING'].replace("'", "''")
+                street = row['STREET'].replace("'", "''")
+                zipcode = row['ZIPCODE'].replace("'", "''")
+                phone = row['PHONE'].replace("'", "''")
+                cuisine_description = row['CUISINE DESCRIPTION'].replace("'", "''")
+                inspection_date = row['INSPECTION DATE'].replace("'", "''")
+                action = row['ACTION'].replace("'", "''")
+                violation_code = row['VIOLATION CODE'].replace("'", "''")
+                violation_desc = row['VIOLATION DESCRIPTION'].replace("'", "''")
+                critical_flag = row['CRITICAL FLAG'].replace("'", "''")
+                score = row['SCORE'].replace("'", "''")
+                grade = row['GRADE'].replace("'", "''")
+                grade_date = row['GRADE DATE'].replace("'", "''")
+                record_date = row['RECORD DATE'].replace("'", "''")
+                inspection_type = row['INSPECTION TYPE'].replace("'", "''")
+                latitude = row['Latitude'].replace("'", "''")
+                longitude = row['Longitude'].replace("'", "''")
+                community_board = row['Community Board'].replace("'", "''")
+                council_district = row['Council District'].replace("'", "''")
+                census_tract = row['Census Tract'].replace("'", "''")
+                bin_number = row['BIN'].replace("'", "''")
+                bbl_number = row['BBL'].replace("'", "''")
+                nta = row['NTA'].replace("'", "''")
+                # try:
+                #     encoded_camis = camis.encode('utf-8', errors='ignore')
+                #     encoded_dba = dba.encode('utf-8', errors='ignore')
+                # except UnicodeEncodeError:
+                #     # Handle or log the error as needed
+                #     continue
+                # Generate the SQL INSERT statement
+                sql = f"INSERT INTO dbo.restaurant_inspection (camis, dba, boro, building, street, zipcode, phone, cuisine, inspection_date, action, violation_code, violation_desc, critical_flag, score, grade, grade_date, record_date, inspection_type, latitude, longitude, community_board, council_district, census_tract, bin_number, bbl_number, nta) VALUES ('{camis}', '{dba}', '{boro}', '{building}', '{street}', '{zipcode}', '{phone}', '{cuisine_description}', '{inspection_date}', '{action}', '{violation_code}', '{violation_desc}', '{critical_flag}', '{score}', '{grade}', '{grade_date}', '{record_date}', '{inspection_type}', '{latitude}', '{longitude}', '{community_board}', '{council_district}', '{census_tract}', '{bin_number}', '{bbl_number}', '{nta}')"
+                # Execute the insert query
+                cursor.execute(sql)
+                # Print the status of the row insertion
+                logging.info(f"Row inserted successfully: {row}")
+                # pause execution for 10 second
+                time.sleep(0.01)
+                # Commit the changes to the database
+                cursor.commit()
+            
 
     except Exception as e:
-        logging.error(f"An error occurred when inserting data into the table, error details: {e},\n{sql}")
+        logging.error(f"An error occurred when inserting data into the table, error details: {e}")
 
     finally:
         # Close the cursor and connection
         cursor.close()
         conn.close()
+        # Close the file after processing
+        csvfile.close()
 else:
     logging.error("Failed to connect to the SQL Server.")
